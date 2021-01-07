af6a9814d1ddcd8e0b397b5d29cf2e2cbaeab4f3
carla regina <carlareginam10@gmail.com>
2021-01-06 23:27:36 -0300

    GET Orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -3,10 +3,9 @@ package com.devsuperior.dsdeliver.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Product;
@@ -16,7 +15,8 @@ import com.devsuperior.dsdeliver.repositories.ProductRepository;
 public class ProductService {
 	@Autowired
 	private ProductRepository repository;
-	//@Transactional(readOnly = true)
+	
+	@Transactional (readOnly = true)
 	public List<ProductDTO> findAll(){
 		List<Product> list = repository.findAllByOrderByNameAsc();;
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());

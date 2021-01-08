13b47f5ae23b8f1fc8d6495a0fe0c472510f1c67
carla regina <carlareginam10@gmail.com>
2021-01-08 16:12:22 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 //import React from 'react';
 import './App.css';
+import Navbar from './Navebar';
 
 
 
 function App() {
   return (
     <div>
-     <h1>Hello</h1>
+     <Navbar/>
     
     </div>
   );
